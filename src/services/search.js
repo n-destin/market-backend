1e8451465f32c754ecd3e0a35651e8bf4940b1af
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2023-07-19 20:27:56 -0400

    some change

--- src/services/search.js
+++
@@ -1,8 +0,0 @@
-import Product from "../models/product";
-
-
-export function Search(searchTerm){
-    Product.createIndexes({productDescription : "text", productName : "text"})
-    const matchProducts = await Product.find({$text : {$search: searchTerm}}).toArray();
-    return matchProducts
-}
